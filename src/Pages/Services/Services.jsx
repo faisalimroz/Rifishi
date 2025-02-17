3ced8003ed7e736bf7ba413d6ceacb8974f88cc7
faisalimroz <faisalimroz49@gmail.com>
2025-02-17 21:06:18 +0600

    second

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from '../../Components/Hero';
+import BusinessCard from '../../Components/BusinessCard/BusinessCard';
 
 const Services = () => {
     return (
         <div>
             <Hero Title={Services}></Hero>
+            //add a card here 
+
+            <BusinessCard></BusinessCard>
         </div>
     );
 };
