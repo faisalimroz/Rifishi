3a968fbe3d26125074cc8888ac5871f54c43d6d2
faisalimroz <faisalimroz49@gmail.com>
2025-02-22 19:08:46 +0600

    logo added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../assets/save.jpg'
-import '../assets/rifi.jpg'
+import bgImage from '../assets/rifi.jpg';
 const Hero = ({Title}) => {
     return (
         <div>
         <div className='overflow-hidden'>
         <div className='bg-fixed bg-cover bg-center bg-no-repeat h-[100vh] w-full flex flex-col items-center justify-center' style={{
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), 
-          url(http://www.asiapacific.com.bd/Admin/Content/T_LOGISTICS-Big7120255533.jpg)`}}>
+          url(${bgImage})`}}>
          <div>
           <h2 className='text-white lg:text-6xl text-3xl text-center'>{Title}</h2>
          </div>
